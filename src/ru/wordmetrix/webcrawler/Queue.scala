43db09d978690fa351a78ae8f2dcbfa5f67b5bb6
electricmind <buy@neural.ru>
2013-10-06 16:25:24 +0400

    Policy of priority queue has been changed.

--- src/ru/wordmetrix/webcrawler/Queue.scala
+++ src/ru/wordmetrix/webcrawler/Queue.scala
@@ -5,6 +5,7 @@ import com.sun.org.apache.xerces.internal.util.URI
 import scala.collection.mutable.PriorityQueue
 import scala.collection.immutable
 import scala.annotation.tailrec
+import ActorDebug._
 /* 
  * Queue is a system of estimation of priorities and queueing task for webget
  */
@@ -13,19 +14,20 @@ class Queue(storage: Storage)(implicit cfg: CFG) extends Actor {
     type Seeds = Set[WebCrawler.Seed]
     type Item = (Double, WebCrawler.Seed, Seeds, V)
 
-    var central = Vector(List[(String, Double)]())
+    var central : Option[V] = None //Vector(List[(String, Double)]())
 
     val queue = new PriorityQueue[Item]()(
-        Ordering.fromLessThan((x: Item, y: Item) => x._1 > y._1))
+        Ordering.fromLessThan((x: Item, y: Item) => x._1 < y._1))
 
     //    @tailrec
     //    private 
 
     val dispatcher: Actor = Actor.actor {
         def dispatch(webgets: immutable.Queue[WebGet], seeds: immutable.Queue[WebCrawler.Seed]): Unit = {
+            log("Dispatch:%s %s",webgets.size,seeds.size)
             Actor.react {
                 case seed: WebCrawler.Seed => {
-                    println("Dispatch: " + seed)
+                    this.debug("%s",seed)
                     webgets match {
                         case immutable.Queue() => dispatch(webgets, seeds.enqueue(seed))
                         case webgets => webgets.dequeue match {
@@ -38,7 +40,7 @@ class Queue(storage: Storage)(implicit cfg: CFG) extends Actor {
                 }
 
                 case None => {
-                    println("dispatch None")
+                    this.debug("%s",None)
                     seeds match {
                         case immutable.Queue() => this ! None
                         case _                 =>
@@ -46,9 +48,8 @@ class Queue(storage: Storage)(implicit cfg: CFG) extends Actor {
                     dispatch(webgets, seeds)
                 }
                 case webget: WebGet => {
-                    println("dispatch: " + webget)
+                    this.debug("%s",webget)
                     seeds match {
-
                         case immutable.Queue() => {
                             this ! None
                             dispatch(webgets.enqueue(webget), seeds)
@@ -72,9 +73,14 @@ class Queue(storage: Storage)(implicit cfg: CFG) extends Actor {
         loop {
             react {
                 case (seeds: Seeds, seed: WebCrawler.Seed, vector: V) => {
-                    println("Get seeds")
-                    val queueitem = (central.normal * vector.normal, seed, seeds, vector)
+                    log("Get seeds from %s seed", seed)
+                    central = central match {
+                        case None => Some(vector)
+                        case Some(v) => Some(v + vector.normal)
+                    }
+                    val queueitem = (central.get.normal * vector.normal, seed, seeds, vector)
 
+                    log("Priority: %f",queueitem._1)
                     if (queue.isEmpty) {
                         queue.enqueue(queueitem)
                         dispatcher ! None
@@ -84,28 +90,30 @@ class Queue(storage: Storage)(implicit cfg: CFG) extends Actor {
                 }
 
                 case (seed: WebCrawler.Seed) => {
-                    println("Get: " + seed)
+                    this.debug("%s",seed)
                     dispatcher ! seed
                 }
 
                 case (webget: WebGet) => {
-                    println("Get: " + webget)
+                    this.debug("%s",webget)
                     dispatcher ! webget
                 }
 
                 case None => {
-                    println("Get NONE")
+                    this.debug("%s",None)
                     if (!queue.isEmpty) {
-                        val (_, seed, seeds: Set[WebCrawler.Seed], vector) = queue.dequeue()
-                        val c = central + vector
+                        val (priority, seed, seeds: Set[WebCrawler.Seed], vector) = queue.dequeue()
+                        log("Priority of request: %f", priority)
+//                        val c = central + vector
+//                        central = c
 
                         //                        println("New vector:  %f".format(c.normal * central.normal))
                         //                        println(seed)
                         //                        println(seeds)
-                        central = c
+                        storage ! seed 
 
                         for (seed <- seeds) {
-                            println("Sent: " + seed)
+                            this.debug("Sent: %s",seed)
                             dispatcher ! seed
                         }
                     } else {
--- src/ru/wordmetrix/webcrawler/WebGet.scala
+++ src/ru/wordmetrix/webcrawler/WebGet.scala
@@ -1,29 +1,30 @@
 package ru.wordmetrix.webcrawler
 
 import scala.actors.Actor
+import ActorDebug._
 /*
- * WebGet sucks an information from webpages 
+ * WebGet gets an information from web pages 
  */
 class WebGet(queue: Queue, gather: Gather)(implicit cfg: CFG) extends Actor {
     def act() = {
         loop {
             react {
                 case seed: WebCrawler.Seed => {
-                    println("webget: " + seed)
+                    log("Get %s", seed)
                     val connection = seed.toURL.openConnection()
                     try {
-                    connection.getContentType().split(";").head match {
-                        case "text/html" => {
-                            gather ! (
-                                seed,
-                                io.Source.fromInputStream(
-                                    connection.getInputStream()).
-                                    getLines().mkString(""))
+                        connection.getContentType().split(";").head match {
+                            case "text/html" => {
+                                gather ! (
+                                    seed,
+                                    io.Source.fromInputStream(
+                                        connection.getInputStream()).
+                                        getLines().mkString(""))
+                            }
+                            case _ => None
                         }
-                        case _ => None
-                    }
-                    Thread.sleep(200)
-                } catch { case x => println(x)}
+//                        Thread.sleep(200)
+                    } catch { case x => println(x) }
                     queue ! this
                 }
             }

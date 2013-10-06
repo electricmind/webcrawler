2436b6381e3ef530567c7ecc4b588b0c01e5b3a8
electricmind <buy@neural.ru>
2013-10-06 16:18:02 +0400

    CFG gets a debug & logging features

--- src/ru/wordmetrix/webcrawler/CFG.scala
+++ src/ru/wordmetrix/webcrawler/CFG.scala
@@ -1,6 +1,7 @@
 package ru.wordmetrix.webcrawler
 
 import java.io.File
+import scala.actors.Actor
 /*
  * CFG: Object that holds a set of the parameters of current session.
  */
@@ -8,13 +9,39 @@ import java.io.File
 
 object CFG {
    def apply() = {
-       new CFG(new File("/tmp/webcrawler"))
+       new CFG(new File("/tmp/webcrawler"),false)
    }
 }
 
-case class CFG(val path : File) {
+case class CFG(val path : File, val isdebug : Boolean) {
   def apply(key: String, value : String) = key match {
-      case "p" => CFG(new File(value)) 
+      case "p" => CFG(new File(value), isdebug)
+      case "d" => CFG(path, value == "t")
   }
 }
 
+object debug {
+    def apply(format : String, p : Any*)(implicit cfg : CFG) = {
+        if (cfg.isdebug) println(format.format(p:_*))
+    }
+
+    def apply(actor : Actor, format : String, p : Any*)(implicit cfg : CFG) = {
+        if (cfg.isdebug) println( "In " + actor + ": " + format.format(p:_*))
+    }
+}
+
+object ActorDebug {
+    implicit def actor2ActorDebug(actor : Actor) = new ActorDebug(actor)
+}
+
+import debug.apply
+
+class ActorDebug(actor : Actor) {
+    def debug(format : String, p : Any*)(implicit cfg : CFG) = apply(actor, format, p:_*)
+}
+
+object log {
+    def apply(format : String, p : Any*)(implicit cfg : CFG) = {
+         println({if (cfg.isdebug) "->>> " else ""} +  format.format(p:_*))
+    }
+}

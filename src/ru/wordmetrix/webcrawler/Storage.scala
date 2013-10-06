0c3a2d198d2a17fa9508ee6e9ad01b3ce961f247
electricmind <buy@neural.ru>
2013-10-06 16:20:05 +0400

    Storage was implemented by a simple manner: it saves data into plain files and moves the significant files to another directory.

--- src/ru/wordmetrix/webcrawler/Storage.scala
+++ src/ru/wordmetrix/webcrawler/Storage.scala
@@ -1,18 +1,46 @@
 package ru.wordmetrix.webcrawler
 
 import scala.actors.Actor
+import ActorDebug._
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.io.FileInputStream
 
-class Storage()(implicit val cfg : CFG) extends Actor {
+class Storage()(implicit val cfg: CFG) extends Actor {
+
+    def seedToFilename(seed: WebCrawler.Seed) = """[/:\\]""".r.replaceAllIn("""https?://""".r.replaceFirstIn(seed.toString, ""), "-") match {
+        case x if x.length > 120 => x.slice(0, 120) +
+            x.slice(0, 120).hashCode.toString
+        case x => x
+    }
+
     def act() = loop {
         react {
-            case seed : WebCrawler.Seed => {
-                //TODO: mark page as significant
-                
+            case seed: WebCrawler.Seed => {
+                log("Store marks datum for %s as significant", seed)
+                val name = seedToFilename(seed)
+                val fin = new FileInputStream(new File(new File(cfg.path,"tmp"),name))
+                val buf = new Array[Byte](fin.available())
+                fin.read(buf)
+                fin.close
+                val fout = new FileOutputStream(new File(cfg.path,name))
+                fout.write(buf)
+                fout.close()
             }
-            
-            case (seed : WebCrawler.Seed, intell : WebCrawler.Intell) => {
-                //TODO : save page in storage
+
+            case (seed: WebCrawler.Seed, intell: WebCrawler.Intell) => {
+                log("Store data for %s", seed)
+
+                val file = new OutputStreamWriter(
+                    new FileOutputStream(
+                        new File(new File(cfg.path,"tmp"), seedToFilename(seed))))
+
+                file.write(intell)
+                file.close()
             }
+
+            case x => log("Store got something strange: %s", x)
         }
     }
 

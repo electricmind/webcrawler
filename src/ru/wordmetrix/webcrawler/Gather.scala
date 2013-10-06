5ef586a7efa0509395090a8d80ea261b137f1ba9
electricmind <buy@neural.ru>
2013-10-06 16:21:56 +0400

    Gather has been customized for the use with Wikipedia: it treats only content area now.

--- src/ru/wordmetrix/webcrawler/Gather.scala
+++ src/ru/wordmetrix/webcrawler/Gather.scala
@@ -7,35 +7,58 @@ import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 import org.xml.sax.InputSource
 import scala.xml.parsing.NoBindingFactoryAdapter
 import java.io.CharArrayReader
-
+import ActorDebug._
+/*
+ * Gather analyzes a page and elicits links and useful load.
+ */
 class Gather(storage: Storage, queue: Queue)(implicit val cfg: CFG)
         extends Actor {
 
-    def page2xml(page: WebCrawler.Page) = (new NoBindingFactoryAdapter).loadXML(
+    val map = scala.collection.mutable.Set[(String, String)]()
+
+    def page2xml_whole(page: WebCrawler.Page) = (new NoBindingFactoryAdapter).loadXML(
         new InputSource(new CharArrayReader(page.toArray)),
         new SAXFactoryImpl().newSAXParser())
 
-    def xml2seeds(xml: scala.xml.Node, base : URI) = (xml \\ "a").
-        map(x => x.attribute("href")).flatten.
-        map(x => base.resolve(x.toString)).toSet
+    def page2xml(page: WebCrawler.Page) : scala.xml.NodeSeq = ((page2xml_whole(page) \\ "div").
+        filter(
+            x => x.attribute("id").getOrElse("").toString == "mw-content-text"))
 
-    def xml2vector(xml: scala.xml.Node) = Vector(
+    def xml2seeds(xml: scala.xml.NodeSeq, base: URI) = (xml \\ "a").
+        map(x => x.attribute("href")).flatten.
+        map(x => base.resolve(x.toString)).
+        filter(x => x.getHost() == "en.wikipedia.org").
+        filter(x => {
+            val id = (x.getHost(), x.getPath())
+            if (map contains id) {
+                false
+            } else {
+                map.+=(id)
+                true
+            }
+        }).toSet
+
+    def xml2vector(xml: scala.xml.NodeSeq) = Vector(
         "\\s+".r.split(xml.text).groupBy(x => x).map({
             case (x, y) => (x -> y.length.toDouble)
         }).toList)
 
-    val xml2intell = xml2vector _
+    def xml2intell(xml: scala.xml.NodeSeq) = xml.text
 
     def act() = loop {
-        react { 
-            case (seed : URI, page: WebCrawler.Page) => {
-                println("gather " + seed)
-                val xml = page2xml(page)
-                storage ! ((seed, xml2intell(xml)))
-                
-//                println("gathered " + (xml2seeds(xml,seed)))
-//                println(xml2vector(xml))
-                queue ! ((xml2seeds(xml,seed), seed, xml2vector(xml)))
+        react {
+            case (seed: URI, page: WebCrawler.Page) => {
+                log("Gather data from %s", seed)
+                try {
+                    val xml = page2xml(page)
+                    storage ! ((seed, xml2intell(xml)))
+
+                    //                println("gathered " + (xml2seeds(xml,seed)))
+                    //                println(xml2vector(xml))
+                    queue ! ((xml2seeds(xml, seed), seed, xml2vector(xml)))
+                } catch {
+                    case x => log("Gathering failed on %s: %s", seed, x)
+                }
             }
         }
     }

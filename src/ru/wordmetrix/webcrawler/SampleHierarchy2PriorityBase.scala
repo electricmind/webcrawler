47cadf65f07a0fe84b2ca57e639aaa2600753d7b
electricmind <buy@neural.ru>
2013-10-20 12:52:25 +0400

    Implement a stub for SampleHierrchy2Priority

---
+++ src/ru/wordmetrix/webcrawler/SampleHierarchy2PriorityBase.scala
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+package ru.wordmetrix.webcrawler
+import WebCrawler._
+import ru.wordmetrix.webcrawler.ActorDebug._
+import scala.actors.Actor
+
+trait SampleHierarchy2PriorityBase extends Actor
+
+class SampleHierarchy2PriorityStub  extends SampleHierarchy2PriorityBase {
+    def act {}
+}

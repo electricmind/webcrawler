3f28ea5e8653d0a26332234a5d82edc418a473d9
electricmind <buy@neural.ru>
2013-11-03 17:01:27 +0400

    Convert from URI to filename and revert.

---
+++ src/ru/wordmetrix/utils/Utils.scala
@@ -0,0 +1,29 @@
+package ru.wordmetrix.utils
+
+import java.net.URI
+import scala.util.matching.Regex.Match
+
+object Utils {
+    def uriToFilename(uri: URI) = """[/:_\-\\]""".r.replaceAllIn(
+        """https?://""".r.replaceFirstIn(uri.toString, ""), x => {println(x); x match {
+            case Match("/")  => "---"
+            case Match("-")  => "--"
+            case Match(":")  => "__"
+            case Match("_")  => "___"
+        }}) match {
+            case x if x.length > 120 => x.slice(0, 120) +
+            x.slice(0, 120).hashCode.toString
+            case x => x
+        }
+
+    def filenameToUri(s: String) =
+        new URI("http://" + """---|--|___|__""".r.replaceAllIn(
+            s,
+            x => { println("!",x); x match {
+                case Match("---")  => "/"
+                case Match("--")   => "-"
+                case Match("__")   => ":"
+                case Match("___") => "_"
+            }}
+        ))
+}

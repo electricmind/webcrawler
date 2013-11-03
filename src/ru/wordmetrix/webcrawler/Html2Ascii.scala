a55c1d26e1d5d9cb9f3bfcaee2eccf83e0cd8b04
electricmind <buy@neural.ru>
2013-11-03 19:20:52 +0400

    Optimize wrap() method and do justify()

--- src/ru/wordmetrix/webcrawler/Html2Ascii.scala
+++ src/ru/wordmetrix/webcrawler/Html2Ascii.scala
@@ -3,11 +3,12 @@ import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 import org.xml.sax.InputSource
 import scala.xml.parsing.NoBindingFactoryAdapter
 import java.io.CharArrayReader
+import scala.util.Random
 
 object Html2Ascii {
-  def apply(page: scala.xml.NodeSeq) = {
-    new Html2Ascii(page)
-  }
+    def apply(page: scala.xml.NodeSeq) = {
+        new Html2Ascii(page)
+    }
 }
 
 class Html2Ascii(page: scala.xml.NodeSeq) {
@@ -18,31 +19,47 @@ class Html2Ascii(page: scala.xml.NodeSeq) {
             new SAXFactoryImpl().newSAXParser()) to //\\ "BODY"
     )
 
-    // q.child.map { case a @ <a>{text}</a> => text + ":" + (a \ "@href").text; case t : xml.Text => t } mkString ""
-
     val div = Set("p", "h1", "h2", "h3", "h4", "h5", "h6", "div")
     val span = Set("i", "a", "b", "span", "sup")
     val discard = Set("img", "script", "form", "input", "button")
-    
-    def wrap(size : Int = 72) :String = dump(page).foldLeft(("",0))( {
-        case ((s,i),' ' | '\n') if i > size => (s + "\n",0)
-        //case ((s,i),'\') if i > size => (s + "\n",0)
-        case ((s,i),'\n')  => (s + '\n',0)
-        case ((s,i),c)  => (s + c,i+1)
-    })._1
-    
-    def dump(nodes: Seq[scala.xml.Node] = page): String = {
-        //println("qq")
-        (nodes map {
 
-            //case a @ <a>{ nodes @ _* }</a> => dump(nodes) + ":" + (a \ "@href")
-            case <h1>{nodes @ _*}</h1> => "\n = " + dump(nodes) + " =\n"
-            case <h2>{nodes @ _*}</h2> => "\n == " + dump(nodes) + " ==\n"
-            case <h3>{nodes @ _*}</h3> => "\n === " + dump(nodes) + " ===\n"
-            case <h4>{nodes @ _*}</h4> => "\n ==== " + dump(nodes) + " ====\n"
-            case <h5>{nodes @ _*}</h5> => "\n ===== " + dump(nodes) + " =====\n"
-            case <h6>{nodes @ _*}</h6> => "\n ====== " + dump(nodes) + " ======\n"
-            
+    def justify(x: String, size: Int = 72) = {
+        val gap = size - x.length
+        x.split(" ") match {
+            case words if words.length <= 1 => x
+            case words =>
+                val q1 = gap / (words.length - 1)
+                val q2 = gap % (words.length - 1)
+                val spaces1 = (1 to words.length).map(x => " " * (q1 + 1))
+                val spaces2 = Random.shuffle(spaces1.take(q2).map(_ + " ") ++ spaces1.drop(q2))
+                println(words.zip(spaces2).map({ case (x, y) => x + y }).mkString)
+                words.zip(spaces2).map({ case (x, y) => x + y }).mkString
+        }
+    }
+
+    def wrap(size: Int = 72) = dump(page).split("\n").map({
+        case x if x.length < size => List(x)
+        case x => {
+            x.split(" ").foldLeft(List[String]()) {
+                case (s :: ss, w) => s + " " + w match {
+                    case s if s.length < size => s :: ss
+                    case _ if w.length > size => w :: justify(s,size) :: ss
+                    case _                    => w :: justify(s,size) :: ss
+                }
+                case (List(), w) => List(w)
+            }.reverse
+        }
+    }).flatten.mkString("\n")
+
+    def dump(nodes: Seq[scala.xml.Node] = page): String = {
+        (nodes map {
+            case <h1>{ nodes @ _* }</h1> => "\n = " + dump(nodes) + " =\n"
+            case <h2>{ nodes @ _* }</h2> => "\n == " + dump(nodes) + " ==\n"
+            case <h3>{ nodes @ _* }</h3> => "\n === " + dump(nodes) + " ===\n"
+            case <h4>{ nodes @ _* }</h4> => "\n ==== " + dump(nodes) + " ====\n"
+            case <h5>{ nodes @ _* }</h5> => "\n ===== " + dump(nodes) + " =====\n"
+            case <h6>{ nodes @ _* }</h6> => "\n ====== " + dump(nodes) + " ======\n"
+
             case <table>{ tr @ _* }</table> => "\n" + "=" * 60 + "\n" + (tr map {
                 case <tr>{ td @ _* }</tr> => td map {
                     case <td>{ nodes @ _* }</td> => dump(nodes) match {

79ceec6af561b9e8c19dc4a69047c92b373d2097
electricmind <buy@neural.ru>
2014-01-11 14:18:40 +0400

    Take previous limit value when new cluster is created

--- src/main/scala/ru/wordmetrix/webcrawler/TargetVector.scala
+++ src/main/scala/ru/wordmetrix/webcrawler/TargetVector.scala
@@ -8,7 +8,7 @@ class TargetVector[F](val average: AverageVector[F],
         this(new AverageVector[F](), List[(Double, Vector[F])](), n)
 
     def factory(average: AverageVector[F], vs: List[(Double, Vector[F])]) =
-        new TargetVector[F](average, vs.map(_._2).map(x => (average.normal * x, x)))
+        new TargetVector[F](average, vs.map(_._2).map(x => (average.normal * x, x)),n)
 
     def +(v1: Vector[F], callback : => Unit = {}): TargetVector[F] = {
         val priority = v1.normal * average.normal

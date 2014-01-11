23f6fc55da9b7965d836cdd289858a510633e049
electricmind <buy@neural.ru>
2014-01-11 12:35:50 +0400

    Rewrite EvaluatePriorityMatrix with akka.actor

--- src/main/scala/ru/wordmetrix/webcrawler/EvaluatePriorityMatrix.scala
+++ src/main/scala/ru/wordmetrix/webcrawler/EvaluatePriorityMatrix.scala
@@ -1,21 +1,53 @@
 package ru.wordmetrix.webcrawler
 
-import scala.actors.Actor
-import ru.wordmetrix.utils.{CFG, CFGAware, log}
+import ru.wordmetrix.utils.{ CFG, CFGAware, log }
 import ru.wordmetrix.utils.ActorDebug.actor2ActorDebug
 import WebCrawler.{ Seed, Word }
 import scala.collection.mutable.PriorityQueue
 import ru.wordmetrix.vector.Vector
 import ru.wordmetrix.utils.debug
-class EvaluatePriorityMatrix(storage: Storage,
-                             sample: SampleHierarchy2PriorityBase)(implicit cfg: CFG) extends Actor
+import java.io.CharArrayReader
+import java.net.URI
+
+import scala.Option.option2Iterable
+import scala.xml.parsing.NoBindingFactoryAdapter
+
+import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+import org.xml.sax.InputSource
+
+import akka.actor.{ Actor, ActorRef, Props, actorRef2Scala }
+import ru.wordmetrix.features.Features
+import ru.wordmetrix.utils.{ CFG, CFGAware, Html2Ascii, debug, log }
+import ru.wordmetrix.vector.Vector
+import ru.wordmetrix.webcrawler.LinkContext.Feature
+
+object EvaluatePriorityMatrix {
+    abstract sealed class EvaluatePriorityMatrixMessage
+
+    case class EvaluatePriorityMatrixSeed(seed: URI)
+
+    def props(storage: Props, gather: Props, seedqueue: Props, sample: Props, cfg: CFG): Props =
+        Props(new EvaluatePriorityMatrix(storage, gather, seedqueue, sample)(cfg))
+}
+
+class EvaluatePriorityMatrix(storageprop: Props,
+                             gatherprop: Props,
+                             seedqueueprop: Props,
+                             sampleprop: Props)(implicit cfg: CFG) extends Actor
         with CFGAware {
     override val name = "Evaluate . Matrix"
+
     type Priority = Double
     type V = ru.wordmetrix.vector.Vector[Word]
     type Item = (Priority, Seed)
     type VItem = (Priority, V)
 
+    import EvaluatePriorityMatrix._
+    import SeedQueue._
+    import Gather._
+    import Storage._
+    import SampleHierarchy2Priority._
+    
     var priorities = Map[Seed, (Priority, Set[Seed])]()
 
     var vectors = Map[Seed, (V, Set[Seed])]()
@@ -23,11 +55,15 @@ class EvaluatePriorityMatrix(storage: Storage,
     val queue = new PriorityQueue[Item]()(
         Ordering.fromLessThan((x: Item, y: Item) => x._1 < y._1))
 
-    val dispatcher = new Dispatcher(this) {
-        start
-    } 
+    val storage = context.actorOf(storageprop)
 
-    var mode = 0
+    val gather = context.actorOf(gatherprop)
+
+    val seedqueue = context.actorOf(seedqueueprop)
+
+    val sample = context.actorOf(sampleprop)
+
+//    var mode = 0
 
     var factor = new V(List())
 
@@ -79,10 +115,10 @@ class EvaluatePriorityMatrix(storage: Storage,
             priorities = priorities + qq
         }
 
-        if (queue.isEmpty) {
+/*        if (queue.isEmpty) {
             this.log("Empty queue, beg dispatcher for webget")
             this ! dispatcher
-        }
+        }*/
 
         queue.clear()
         for ((seed, (p, seeds)) <- priorities) {
@@ -90,11 +126,11 @@ class EvaluatePriorityMatrix(storage: Storage,
         }
     }
 
-    def estimate(seed: Seed, v: V) = debug.time("estimate, average size: %s, target size: %s".format(average.vector.size,  target.average.vector.size)) {
+    def estimate(seed: Seed, v: V) = debug.time("estimate, average size: %s, target size: %s".format(average.vector.size, target.average.vector.size)) {
         average = average + v
         target = target + (v, {
             this.debug("accepted %s with %s in %s", seed, v * target.average.normal, target.priority())
-//            storage ! seed
+            //            storage ! seed
         })
 
         newfactor = target.normal - average.normal
@@ -107,101 +143,98 @@ class EvaluatePriorityMatrix(storage: Storage,
         )
     }
 
-    def act() = loop {
-        react {
-            case (seed: Seed) => {
-                this.log("Initial seed: %s", seed)
-                dispatcher ! seed
-            }
-
-            case (seeds: Set[Seed], seed: Seed, v: V) => mode = mode match {
-                case 0 => {
-                    //Initialization
-                    val v1 = v.normal
-                    this.log("Initial phase, v = %s, n = %s, seed = %s",
-                        v.norm, seeds.size, seed)
-                    central = v1
-                    target = target + v1
-                    average = average + v1
-
-                    for (seed <- seeds) {
-                        dispatcher ! seed
-                    }
-
-                 //   storage ! seed
-                    1
-                }
-
-                case 1 => {
-                    //Accumulate initial vectors for target
-                    estimate(seed, v.normal)
-                    this.log("Targeting phase, attitude = %s, n = %s, " +
-                        "seed = %s",
-                        newfactor * central, seeds.size, seed)
-
-                    this.debug("target - average| = %s",
-                        (target.normal - average.normal).norm)
-
-                    this.debug("target*central = %s",
-                        target.normal * central)
-
-                    factor = newfactor
-                    enqueue(seeds, seed, v)
-                    queue.clear()
-
-                    if (factor * central > cfg.targeting) {
-                        priorities = calculate(newfactor, vectors)
-                        //target = new TargetVectorCluster[String](target, n = cfg.targets)
-                        2
-                    } else {
-                        1
-                    }
-                }
-
-                case 2 => {
-                    //Do work
-                    this.log("Estimating phase,  attitude = %s, priority = %s, seed = %s",
-                        newfactor * central, factor * v.normal, seed)
-                    estimate(seed, v.normal)
-                    if (newfactor.normal * factor.normal < limit) {
-                        this.debug("Priorities should be recalculated")
-                        priorities = calculate(newfactor, vectors)
-                        factor = newfactor
-                    }
-
-                    enqueue(seeds, seed, v)
-                    //sample ! (seed, factor * v.normal)
-                    2
-                }
-            }
-
-            case dispatcher: Dispatcher => {
-                this.debug("Get dispather request")
-                if (!queue.isEmpty) {
-                    val (p, seed) = queue.dequeue
-                    val (_, seeds) = priorities(seed)
-                    priorities = priorities - seed
-                    vectors = vectors ++ {
-                        seeds.filter(vectors contains _).map(x => x -> {
-                            val (vector, seeds) = vectors(x)
-                            (vector, seeds - seed)
-                        })
-                    }
-                    this.log("Request, priority = %s for %s : %s", p,
-                        seed, seeds.headOption.getOrElse("empty"))
-                    dispatcher ! seed
-                } else {
-                    this.debug("Queue was empty")
-                }
-            }
-
-            case None => {
-                this ! dispatcher
-            }
-
-            case webget: WebGet => dispatcher ! webget
-            case x => this.debug("!!!! Unknown message %s from %s",
-                x, sender)
+    def receive(): Receive = {
+        case EvaluatePriorityMatrixSeed(seed: Seed) => {
+            this.log("Initial seed: %s", seed)
+            seedqueue ! SeedQueueRequest(seed)
+            context.become(phase_initialization,false)
         }
+    }
+
+    def phase_initialization(): Receive = {
+        case Gather.GatherSeeds(seed, seeds, v) => {
+            //Initialization
+            val v1 = v.normal
+            this.log("Initial phase, v = %s, n = %s, seed = %s",
+                v.norm, seeds.size, seed)
+            central = v1
+            target = target + v1
+            average = average + v1
+
+            for (seed <- seeds) {
+                seedqueue ! SeedQueueRequest(seed)
+            }
+
+            storage ! StorageSign(seed)
+            context.become(phase_targeting, false)
+
+        }
+    }
+
+    def phase_targeting(): Receive = {
+        //Accumulate initial vectors for target
+        case Gather.GatherSeeds(seed, seeds, v) => {
+
+            estimate(seed, v.normal)
+            this.log("Targeting phase, attitude = %s, n = %s, " +
+                "seed = %s",
+                newfactor * central, seeds.size, seed)
+
+            this.debug("target - average| = %s",
+                (target.normal - average.normal).norm)
+
+            this.debug("target*central = %s",
+                target.normal * central)
+
+            factor = newfactor
+            enqueue(seeds, seed, v)
+            queue.clear()
+
+            if (factor * central > cfg.targeting) {
+                priorities = calculate(newfactor, vectors)
+                //target = new TargetVectorCluster[String](target, n = cfg.targets)
+                context.become(phase_estimating, false)
+            }
+        }
+    }
+
+    def phase_estimating(): Receive = {
+        case Gather.GatherSeeds(seed, seeds, v) => {
+            //Do work
+            this.log("Estimating phase,  attitude = %s, priority = %s, seed = %s",
+                newfactor * central, factor * v.normal, seed)
+            estimate(seed, v.normal)
+            if (newfactor.normal * factor.normal < limit) {
+                this.debug("Priorities should be recalculated")
+                priorities = calculate(newfactor, vectors)
+                factor = newfactor
+            }
+
+            enqueue(seeds, seed, v)
+            sample ! SampleHirarchy2PriorityPriority(seed, factor * v.normal)
+        }
+
+        case SeedQueueGet => {
+            this.debug("Get dispather request")
+            if (!queue.isEmpty) {
+                val (p, seed) = queue.dequeue
+                val (_, seeds) = priorities(seed)
+                priorities = priorities - seed
+                vectors = vectors ++ {
+                    seeds.filter(vectors contains _).map(x => x -> {
+                        val (vector, seeds) = vectors(x)
+                        (vector, seeds - seed)
+                    })
+                }
+                this.log("Request, priority = %s for %s : %s", p,
+                    seed, seeds.headOption.getOrElse("empty"))
+                sender ! seed
+            } else {
+                this.debug("Queue was empty")
+            }
+        }
+       case x => this.debug("!!!! Unknown message %s from %s",
+            x, sender)
+
     }
 }

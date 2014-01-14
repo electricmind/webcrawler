44fc3c4dd7c96bcc605915a0d190ca047e784da5
electricmind <buy@neural.ru>
2014-01-14 22:08:48 +0400

    :)

--- src/main/scala/ru/wordmetrix/webcrawler/EvaluatePriorityMatrix.scala
+++ src/main/scala/ru/wordmetrix/webcrawler/EvaluatePriorityMatrix.scala
@@ -30,13 +30,21 @@ object EvaluatePriorityMatrix {
     object PQ {
         import scalaz._
         import std.option._, std.list._
+
         def apply[U]()(implicit o: scalaz.Order[U]) = Heap.fromData(List[U]())
-        def apply[U](i1: U)(implicit o: scalaz.Order[U]) = Heap.fromData(List[U](i1))
-        def apply[U](x1: U, x2: U, xs: U*)(implicit f: scalaz.Foldable[List], o: scalaz.Order[U]) =
+
+        def apply[U](i1: U)(implicit o: scalaz.Order[U]) =
+            Heap.fromData(List[U](i1))
+            
+        def apply[U](x1: U, x2: U, xs: U*)(
+            implicit f: scalaz.Foldable[List], o: scalaz.Order[U]) =
             xs.foldLeft(Heap.fromData(List(x1, x2))) {
                 case (heap, x) => heap.insert(x)
             }
-        def apply[U](i1: U, heap: Heap[U])(implicit o: scalaz.Order[U]) = heap.insert(i1)
+        
+        def apply[U](i1: U, heap: Heap[U])(
+            implicit o: scalaz.Order[U]) = heap.insert(i1)
+            
         def unapply[U](heap: Heap[U]) =
             if (heap.isEmpty) None else Some((heap.minimum, heap.deleteMin))
     }

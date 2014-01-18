29073065f8a35db67b7a10f63bc602d9ae237005
electricmind <buy@neural.ru>
2014-01-18 17:16:01 +0400

    Factor out code into ArrangeTextDumpClusters

---
+++ src/main/scala/ru/wordmetrix/treeapproximator/package.scala
@@ -0,0 +1,19 @@
+package ru.wordmetrix
+
+import java.io.File
+package object treeapproximator {
+   type ??? = Nothing
+   
+   type *** = Any
+   
+   type Word = Int
+//   type Name = File
+   
+   type Node = TreeApproximator.Node[Word, File]
+   type Tree = TreeApproximator.Tree[Word, File]
+   type Leaf = TreeApproximator.Leaf[Word, File]
+   
+   
+   
+   
+}

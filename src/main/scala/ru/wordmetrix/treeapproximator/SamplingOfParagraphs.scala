aba86c94ec705a42ef5ac1ad69084086843a4aca
electricmind <buy@neural.ru>
2014-01-22 13:12:27 +0400

    Split by regexp pattern

--- src/main/scala/ru/wordmetrix/treeapproximator/SamplingOfParagraphs.scala
+++ src/main/scala/ru/wordmetrix/treeapproximator/SamplingOfParagraphs.scala
@@ -10,63 +10,65 @@ import scala.concurrent.Await
 import scala.concurrent._
 import scala.concurrent.duration._
 
-
 object SamplingOfParagraphs extends App {
-
     override def main(args: Array[String]) {
-        val (file, path) = (args match {
-            case Array() =>
-                printf("Enter SamplingOfParagraphs <uri> [<path>]\n")
+        val (regexp, file, path) = (args match {
+            case Array("-regexp", regexp, file) => (regexp.r, file, ".")
+
+            case Array("-mark", mark, file) =>
+                (s".+$mark.+".r, file, ".")
+
+            case Array(file)                          => ("^\\s+$".r, file, ".")
+
+            case Array("-regexp", regexp, file, path) => (regexp.r, file, path)
+
+            case Array("-mark", mark, file, path) =>
+                (s".+$mark.+".r, file, path)
+
+            case Array(file, path) => ("^\\s+$".r, file, path)
+
+            case _ =>
+                printf("Enter "
+                    + "SamplingOfParagraphs [-mark <Mark>] <uri> [<path>]\n")
                 sys.exit
-                (null, null)
-            case Array(file)       => (file, ".")
-            case Array(file, path) => (file, path)
+                (null, null, null)
 
         }) match {
-            case (file, path) => (new URI(file), new File(path))
+            case (regexp, file, path) => (regexp, new URI(file), new File(path))
         }
 
         def write(paragraph: List[String], n: Int) = if (paragraph.length > 10)
             (path /
-                s"${file.getPath().split("/").toList.last}.%03d.txt".format(n))
-                .write(s"page $n\n" + paragraph.mkString("\n") + "\n")
+                s"${file.getPath().split("/").toList.last}.%04d.txt".format(n))
+                .write(s"$n : ${file.getPath().split("/").toList.last} : "
+                    + paragraph.mkString("\n") + "\n")
 
-        def output(lines: List[String], n: Int): Unit = if (n < 10000) lines match {
-            case List() =>
+        def output(lines: List[String], n: Int): Unit = if (n < 10000)
+            lines match {
+                case paragraph @ line :: List() => write(paragraph, n)
 
-            case list =>
-                list.span(!_.trim.isEmpty()) match {
+                case line :: lines =>
+                    println(line)
+                    lines.span(regexp.findFirstMatchIn(_).isEmpty) match {
+                        case (List(), List()) =>
 
-                    case (List(), List()) =>
+                        case (paragraph @ List(_@ _*), lines) =>
+                            write(line :: paragraph, n)
+                            output(lines, n + 1)
+                    }
 
-                    case (paragraph @ List(_@ _*), _ :: lines) =>
-                        write(paragraph, n)
-                        output(lines, n + 1)
-
-                    case (paragraph @ List(_@ _*), List()) =>
-                        write(paragraph, n)
-                }
-        }
+            }
 
         val end = for {
-            
-             content <- Future(file.toURL.getContent())
-             lines <- content match {
-                 case content : InputStream => Future(io.Source.fromInputStream(content).getLines.toList)
-                 case _ =>                 Future.failed(new Exception("Invalid data type"))
-
-             }
-
- /*           case Success(content) =>
-                println(1)
-                Future.failed(new Exception("Invalid data type"))
-
-            case f @ Failure(t) =>
-                println(2)
-                
-                Future.failed(t)*/
+            content <- Future(file.toURL.getContent())
+            lines <- content match {
+                case content: InputStream =>
+                    Future(io.Source.fromInputStream(content).getLines.toList)
+                case _ =>
+                    Future.failed(new Exception("Invalid data type"))
+            }
         } yield {
-             output(lines, 1)
+            output(lines, 1)
         }
 
         Await.ready(end, 1 minute)
